359ad8c1767b54f7ada513c6ed4210f59122059f
huangivy <ihuang252@student.fuhsd.org>
2017-05-15 13:06:40 -0700

    Update ScoreScreen.java

--- src/ScoreScreen.java
+++ src/ScoreScreen.java
@@ -1,4 +1,18 @@
+import java.awt.Dimension;
 
-public class ScoreScreen {
+import javax.swing.JPanel;
 
+public class ScoreScreen extends JPanel {
+
+	private JPanel score;
+	public static final int WIDTH = 300;
+	public static final int HEIGHT = 500;
+	
+	public ScoreScreen(){
+		score = new JPanel();
+		score.setMinimumSize(new Dimension(100, 300));
+		score.setSize(WIDTH, HEIGHT);
+		score.setVisible(true);
+		
+	}
 }
