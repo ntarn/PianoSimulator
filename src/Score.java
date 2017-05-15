804ab661559836187dc460918bb562895b9f75b4
ntarn <tarn.natalie@gmail.com>
2017-05-15 09:24:17 -0700

    Add files via upload
    java docs

--- src/Score.java
+++ src/Score.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Score {
-
+	private ArrayList<Double> score;
+	/**
+	 * This constructor creates a list of scores that a player receives after playing a piece.
+	 */
+	public Score(){
+		score= new ArrayList<Double>();
+	}
+	/**
+	 * This method calculates the accuracy of how accurate the player played the piano piece.
+	 * @return score the percentage of the accuracy
+	 */
+	public double calcScore(){
+		
+		
+		return 0;
+	}
+	/**
+	 * This method ranks all the player's scores in order in the ArrayList.
+	 */
+	private void rankScore(){
+		
+	}
 }
