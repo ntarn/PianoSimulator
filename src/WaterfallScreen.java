1353824d8305b38ec53dd054c954631d3012439d
huangivy <ihuang252@student.fuhsd.org>
2017-05-15 13:05:46 -0700

    Update WaterfallScreen.java

--- src/WaterfallScreen.java
+++ src/WaterfallScreen.java
@@ -1,4 +1,18 @@
+import java.awt.Dimension;
 
-public class WaterfallScreen {
+import javax.swing.JPanel;
 
+public class WaterfallScreen extends JPanel{
+
+	private JPanel waterfall;
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	
+	public WaterfallScreen(){
+		waterfall = new JPanel();
+		waterfall.setMinimumSize(new Dimension(500,400));
+		waterfall.setSize(WIDTH, HEIGHT);
+		waterfall.setVisible(true);
+	}
+	
 }
